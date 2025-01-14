037d2f6d683060731031f58b5e4ed96bda905f16
abdulghafar12 <poetryagu@gmail.com>
2025-01-14 22:18:16 +0500

    Add files via upload

---
+++ Proq#3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // task3
+int priA=50.0, priB=30.0, priC=20.0  ;
+int qunA , qunB , qunC ;
+cout<<"The price of A ,B, C  is :  "<<" \t"<<priA<<"\t"<<priB<<"\t"<<priC<<endl;
+cout<<" Enter the quantity of A ,B & C"<<endl;
+cin>>qunA;
+cout<<endl;
+cin>>qunB;
+cout<<endl;
+cin>>qunC;
+double total = ( qunA*priA) + ( qunB*priB) + ( qunC*priC);
+cout<<"The total price is "<<total<<endl;
+double ftotal = (total >= 200)?total * 0.9: total;
+cout<<" Total Price after discount :  "<<ftotal<<endl;
+int shiping = ( ftotal < 150)? 15 : 0.0 ;
+cout<<" Total Price after discount & shiping : " <<ftotal + shiping<<endl;
+}
